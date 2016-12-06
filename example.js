a3e044b441510aef2c3e3ef48375b3b65da0f7d0
Wyatt Preul <wpreul@gmail.com>
2016-12-06 17:29:30 -0600

    Support metadata encryption

--- example.js
+++ example.js
@@ -16,15 +16,19 @@ const client = Manta.createClient({
 
 
 const file = Fs.createReadStream(__dirname + '/README.md');
-const path = '~~/stor/encrypted';
+const path = '~~/stor/enc';
 const key = 'FFFFFFFBD96783C6C91E2222';   // 24 bytes
+const headers = {
+  'm-encrypt-metadata': 'my secret message',
+  'm-encrypt-metadata-cipher': 'aes/192/cbc'
+};
 
 const getKey = function (keyId, callback) {
   return callback(null, key);
 };
 
 
-ClientEncryption.put(path, file, { client, key, keyId: 'dev/test', cipher: 'aes/192/cbc' }, (err) => {
+ClientEncryption.put(path, file, { client, key, keyId: 'dev/test', cipher: 'aes/192/cbc', headers }, (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
@@ -36,6 +40,7 @@ ClientEncryption.put(path, file, { client, key, keyId: 'dev/test', cipher: 'aes/
       process.exit(1);
     }
 
+    console.log(res.headers);
     stream.pipe(process.stdout);
     setImmediate(() => {
       process.stdout.write('\n');
